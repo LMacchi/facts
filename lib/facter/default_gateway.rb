b4ea79926d22e308bf0d136ccf3d3444e92092ec
Laura Macchi <lau.macchi@gmail.com>
2017-05-05 14:59:47 -0400

    Rename gateway to default_gateway

--- lib/facter/default_gateway.rb
+++ lib/facter/default_gateway.rb
@@ -1,4 +1,4 @@
-Facter.add('gateway') do
+Facter.add('default_gateway') do
   confine :kernel => 'Linux'
   setcode do
     Facter::Core::Execution.exec('ip route | grep default | awk \'{print $3}\'')
