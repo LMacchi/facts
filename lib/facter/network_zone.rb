e55eec012949985e86c1d04faf98a215448e5001
LMacchi <lau.macchi@gmail.com>
2017-10-16 16:22:52 -0500

    Add new facts

---
+++ lib/facter/network_zone.rb
@@ -0,0 +1,15 @@
+require "ipaddr"
+Facter.add(:network_zone) do
+  setcode do
+    result = 'dmz'
+    network = Facter.value(:networking)['interfaces'].each do |iface, values|
+      if IPAddr.new("172.16.0.0/16").include?(values['ip'])
+        result = 'internal'
+        break # I found the internal ip, don't bother continuing
+      else
+        next
+      end
+    end
+  result
+  end
+end
